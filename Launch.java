ee76040eed0fa7707b5f3a107dd5f7fbaab6fe13
anishgoyal0603 <anishgoyal0603@gmail.com>
2025-07-10 20:28:05 +0530

    first ever commit

---
+++ Launch.java
@@ -0,0 +1,8 @@
+class launch
+{
+    public static void main (String []args)
+  {
+     System.out.println("Exploring Git and Github 1")
+     System.out.println("Exploring Git and Github 2")
+  }
+}
